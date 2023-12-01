2476e5a084b12c93e86bb3c79e887704c3465ea6
Saad <sk5908774@gmail.com>
2023-12-02 03:39:18 +0500

    updated_code confirm booking change

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -523,7 +523,18 @@ const deleteBooking = asyncHandler(async (req, res) => {
 
 const bookingConfirm = asyncHandler(async (req, res) => {
   console.log(req.body);
+let bookingFind , user ;
 
+  if(req.body.type === "manual")
+  {
+    bookingFind = await Booking.findOne({
+      _id: req.body.booking_Id,
+      isCheckIn: false,
+      isCancel: false,
+      isDeleted: false,
+    }).populate("store_Id");  
+  }
+  else{
   const user = await User.findOne({
     _id: req.body.user_Id,
     role: "user",
@@ -537,13 +548,19 @@ const bookingConfirm = asyncHandler(async (req, res) => {
       .status(404)
       .send({ status: false, message: "User does not exists" });
   }
-  const bookingFind = await Booking.findOne({
+  
+   bookingFind = await Booking.findOne({
     _id: req.body.booking_Id,
     user_Id: req.body.user_Id,
     isCheckIn: false,
     isCancel: false,
     isDeleted: false,
   }).populate("store_Id");
+ 
+}
+
+
+
 
   if (!bookingFind) {
     return res
@@ -590,14 +607,18 @@ const bookingConfirm = asyncHandler(async (req, res) => {
     body: `Your appointment at ${bookingFind?.store_Id?.name} on ${booking?.date} has been successfully booked. We look forward to serving you!`,
   };
 
-  await firebaseNotification(
-    notification,
-    [user],
-    "news",
-    "Specific-User",
-    "system",
-    "users"
-  );
+  if(req.body.type !== "manual")
+  {
+    await firebaseNotification(
+      notification,
+      [user],
+      "news",
+      "Specific-User",
+      "system",
+      "users"
+    );
+  }
+
 
   return res
     .status(200)
