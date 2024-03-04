8474fadcd6ec2c7c2df3a929743241ddea89d716
Saadpluton1 <saad@pluton.ltd>
2024-03-04 12:28:36 +0500

    updated code fixed issue

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -670,7 +670,7 @@ const getStoreGraphsData = asyncHandler(async (req, res) => {
       // Monthly Earnings
       if (bookingDate.getFullYear() === requestedYear) {
         const monthDifference = currentDate.getMonth() - bookingDate.getMonth();
-        const monthIndex = 11 - monthDifference;
+        const monthIndex = bookingDate.getMonth();
 
         if (monthIndex >= 0 && monthIndex < 12) {
           monthlyEarnings[monthIndex] += booking.amount;
