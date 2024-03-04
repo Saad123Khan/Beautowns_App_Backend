cad1c3d504cc7b578437f2d44773733498649e8c
Saadpluton1 <saad@pluton.ltd>
2024-03-04 12:34:06 +0500

    updated code fixed issue

--- controllers/staff.controller.js
+++ controllers/staff.controller.js
@@ -633,7 +633,7 @@ const getStaffGraph = asyncHandler(async (req, res) => {
       // Monthly Earnings
       if (bookingDate.getFullYear() === requestedYear) {
         const monthDifference = currentDate.getMonth() - bookingDate.getMonth();
-        const monthIndex = 11 - monthDifference;
+        const monthIndex = bookingDate.getMonth();
 
         if (monthIndex >= 0 && monthIndex < 12) {
           monthlyEarnings[monthIndex] += booking.amount;
